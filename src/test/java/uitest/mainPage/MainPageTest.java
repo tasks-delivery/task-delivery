05d80cefbe63513a8d08c302e0f9186d984ab2e5
Fiodar Motin <ordeh@mail.ru>
2017-12-02 01:44:50 +0300

    ss

--- src/test/java/uitest/mainPage/MainPageTest.java
+++ src/test/java/uitest/mainPage/MainPageTest.java
@@ -21,7 +21,7 @@ public class MainPageTest extends BaseTest {
     public void firstTest(){
         open(baseUrl);
         $(mainPage.btnContacts).click();
-        $(secondPage.btnAddContacts).shouldNotBe(visible);
+        $(secondPage.btnAddContacts).shouldBe(visible);
 
     }
 
