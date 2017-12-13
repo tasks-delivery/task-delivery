c6da51400691d3c556dcbd83aa619142b9d55d1c
Fiodar Motin <ordeh@mail.ru>
2017-12-14 00:24:33 +0300

    Release 0.2.3

--- src/main/java/uitest/config/ConciseAPI.java
+++ src/main/java/uitest/config/ConciseAPI.java
@@ -9,33 +9,22 @@ import static com.codeborne.selenide.Configuration.browser;
 public abstract class ConciseAPI {
 
     public WebDriver driver;
-/*
-    @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        System.setProperty("webdriver.ie.driver", "src\\main\\resources\\IEDriverServer.exe");
-        Configuration.browser = "chrome";
-        Configuration.headless = true;
-        Configuration.timeout = 8000;
-        Configuration.baseUrl = "http://localhost:8080";
-    }
-   */
 
     static{
         System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
         System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "warn");
     }
 
-        @Parameters({"browser", "browserVersion", "remote", "timeout", "baseUrl"})
+        @Parameters({"browser", "browserVersion", "remote", "headless", "timeout", "baseUrl"})
         @BeforeClass
-        public void setUp(String browsers, String version, String server, Integer time, String url) {
+        public void setUp(String browsers, String version, String server, Boolean mode, Integer time, String url) {
         System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         System.setProperty("webdriver.ie.driver", "src\\main\\resources\\IEDriverServer.exe");
         browser = browsers;
         Configuration.browserVersion = version;
         Configuration.remote = server;
+        Configuration.headless = mode;
         Configuration.timeout = time;
         Configuration.baseUrl = url;
         }
