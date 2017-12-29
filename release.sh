913692c100ac637a18d7830bcb865fef43bd8226
Fiodar Motin <ordeh@mail.ru>
2017-12-29 14:44:26 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 java -jar ./app/target/app-1.jar
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
   mvn clean -rf :app
 fi
+
+exit
 
 
