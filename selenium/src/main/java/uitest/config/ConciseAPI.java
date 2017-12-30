92eec9bdea1800af58d2dc5aa977f25168715730
Fiodar Motin <ordeh@mail.ru>
2017-12-30 21:00:34 +0300

    test

--- selenium/src/main/java/uitest/config/ConciseAPI.java
+++ selenium/src/main/java/uitest/config/ConciseAPI.java
@@ -16,30 +16,25 @@ public abstract class ConciseAPI  {
         System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\winDrivers\\chromedriver.exe");
         System.setProperty("webdriver.ie.driver", "src\\main\\resources\\IEDriverServer.exe");
-
      //   DesiredCapabilities capabilities = new DesiredCapabilities();
     //    capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, false);
-        Configuration.browser = "chrome";
+        Configuration.browser = "firefox";
         Configuration.remote = "http://localhost:4444/wd/hub";
-        Configuration.headless = true;
+     //   Configuration.headless = false;
         Configuration.timeout = 4000;
        // Configuration.baseUrl = "http://mail.ru/";
-        Configuration.baseUrl = "http://127.0.0.1:8080/";
-
-
+        Configuration.baseUrl = "http://localhost:8080/";
     }
 */
 
-    @Parameters({"browser", "browserVersion", "remote", "headless", "timeout", "baseUrl"})
+    @Parameters({"browser", "browserVersion", "timeout", "baseUrl"})
     @BeforeClass
-    public void setUp(String browsers, String version, String server, Boolean mode, Integer time, String url) {
-    System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
-    System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\winDrivers\\chromedriver.exe");
-    System.setProperty("webdriver.ie.driver", "src\\main\\resources\\IEDriverServer.exe");
+    public void setUp(String browsers, String version, Integer time, String url) {
+  //  System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
+  //  System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\winDrivers\\chromedriver.exe");
+   // System.setProperty("webdriver.ie.driver", "src\\main\\resources\\IEDriverServer.exe");
     Configuration.browser = browsers;
     Configuration.browserVersion = version;
-    Configuration.remote = server;
-    Configuration.headless = mode;
     Configuration.timeout = time;
     Configuration.baseUrl = url;
     }
