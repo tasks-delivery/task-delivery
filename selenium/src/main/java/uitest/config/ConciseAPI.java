82920429585f13e051f24c6d31e50c25b36b748c
Fiodar Motin <ordeh@mail.ru>
2017-12-30 12:32:08 +0300

    test

--- selenium/src/main/java/uitest/config/ConciseAPI.java
+++ selenium/src/main/java/uitest/config/ConciseAPI.java
@@ -2,12 +2,33 @@ package uitest.config;
 
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 public abstract class ConciseAPI  {
 
     public WebDriver driver;
+/*
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\winDrivers\\chromedriver.exe");
+        System.setProperty("webdriver.ie.driver", "src\\main\\resources\\IEDriverServer.exe");
+
+     //   DesiredCapabilities capabilities = new DesiredCapabilities();
+    //    capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, false);
+        Configuration.browser = "chrome";
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.headless = true;
+        Configuration.timeout = 4000;
+       // Configuration.baseUrl = "http://mail.ru/";
+        Configuration.baseUrl = "http://127.0.0.1:8080/";
+
+
+    }
+*/
 
     @Parameters({"browser", "browserVersion", "remote", "headless", "timeout", "baseUrl"})
     @BeforeClass
@@ -22,5 +43,6 @@ public abstract class ConciseAPI  {
     Configuration.timeout = time;
     Configuration.baseUrl = url;
     }
+
 
 }
