990cc86e7759361fa8c4790a9fc028d037352cdb
Fiodar Motin <ordeh@mail.ru>
2018-01-01 20:05:31 +0300

    Release 0.2.6

--- src/main/java/selenium/contacts/ContactsListPage.java
+++ src/main/java/selenium/contacts/ContactsListPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.config.BasePage;
-
 import static com.codeborne.selenide.Selenide.$;
 
 public class ContactsListPage extends BasePage {
