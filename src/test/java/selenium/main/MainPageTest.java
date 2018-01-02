581ed788cc1f6ca3e91f4b4f5b04c98f5c238499
Fiodar Motin <ordeh@mail.ru>
2018-01-02 17:09:03 +0300

    Rework tests for new design

---
+++ src/test/java/selenium/main/MainPageTest.java
@@ -0,0 +1,63 @@
+package selenium.main;
+
+import org.testng.annotations.Test;
+import selenium.config.BaseTest;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPageTest extends BaseTest {
+
+    MainPage mainPage;
+
+    public MainPageTest(){
+        mainPage = new MainPage(driver);
+    }
+
+    @Test(description = "Check text near picture")
+    public void textNearPictureShouldBeVisible(){
+        open(baseUrl);
+        $(mainPage.infoText).shouldHave(text("Generates progress reports"))
+        .shouldHave(text("Keeps track of the progress through testing scenarios"))
+        .shouldHave(text("Combines test specs into test coverage scenarios"))
+        .shouldHave(text("Manage requirements, features and use-cases"));
+    }
+
+    @Test(description = "Verify menu item in top bar")
+    public void menuItemInTopBarShouldVisible(){
+        open(baseUrl);
+        $(mainPage.btnAbout).shouldBe(visible).shouldHave(text("About")).exists();
+        $(mainPage.btnBlog).shouldBe(visible).shouldHave(text("Blog")).exists();
+        $(mainPage.btnDocs).shouldBe(visible).shouldHave(text("Docs")).exists();
+        $(mainPage.btnSignIn).shouldBe(visible).shouldHave(text("Sign In")).exists();
+        $(mainPage.btnSupport).shouldBe(visible).shouldHave(text("Support")).exists();
+        $(byText("Task-delivery")).shouldBe(visible);
+        $(mainPage.tdLogo).shouldBe(visible).exists();
+    }
+
+    @Test(description = "Verify big button 'Registration' and 'How it works'")
+    public void buttonsRegistrationAndHowItWorksShouldBeVisible(){
+        open(baseUrl);
+        $(mainPage.btnRegistration).shouldBe(visible).shouldHave(text("Registration")).exists();
+        $(mainPage.btnHowItWork).shouldBe(visible).shouldHave(text("How it works")).exists();
+    }
+
+    @Test(description = "Picture with application should be visible")
+    public void pictureWithApplication(){
+        open(baseUrl);
+        $(mainPage.applicationPicture).shouldBe(visible).exists();
+    }
+
+    @Test(description = "Pictures near text should be visible")
+    public void picturesNearTextInfo(){
+        open(baseUrl);
+        $(mainPage.bugPicture).shouldBe(visible).exists();
+        $(mainPage.managePicture).shouldBe(visible).exists();
+        $(mainPage.scenariosPicture).shouldBe(visible).exists();
+        $(mainPage.specsPicture).shouldBe(visible).exists();
+    }
+
+}
--- src/test/java/selenium/organizations/OrganizationsListPageTest.java
+++
@@ -1,7 +0,0 @@
-package selenium.organizations;
-
-import selenium.config.BaseTest;
-
-public class OrganizationsListPageTest extends BaseTest {
-
-}
