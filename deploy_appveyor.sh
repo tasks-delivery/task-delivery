1a330ca5a9782710418a121de4116818796562ec
Fiodar Motin <ordeh@mail.ru>
2018-04-07 19:51:46 +0300

    set permission for deploy script

--- deploy_appveyor.sh
+++ deploy_appveyor.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -X POST -H "Authorization:Bearer 3mb5uv6nmjl3l80ebsat" -H "Content-Type:application/json" -d "{\"accountName\": \"ordeh\",\"projectSlug\": \"task-delivery\"}" https://ci.appveyor.com/api/builds
+curl -X POST -H "Authorization:Bearer $APPVEYOR_TOKEN" -H "Content-Type:application/json" -d "{\"accountName\": \"ordeh\",\"projectSlug\": \"task-delivery\"}" https://ci.appveyor.com/api/builds
 
