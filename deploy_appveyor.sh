3b6f74fd69375919ec95e10553d84474d78eee0b
Fiodar Motin <ordeh@mail.ru>
2018-04-07 20:54:54 +0300

    test

--- deploy_appveyor.sh
+++ deploy_appveyor.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-curl -X POST -H "Authorization:Bearer $APPVEYOR_TOKEN" -H "Content-Type:application/json" -d "{\"accountName\": \"ordeh\",\"projectSlug\": \"task-delivery\"}" https://ci.appveyor.com/api/builds
-
+curl -X POST -H "Authorization:Bearer $APPVEYOR_TOKEN" -H "Content-Type:application/json" -d "{\"accountName\": \"$APPVEYOR_USERNAME\",\"projectSlug\": \"task-delivery\"}" https://ci.appveyor.com/api/builds
