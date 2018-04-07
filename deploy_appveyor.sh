77c0d691f51a07863d8506f615115bc9a25c5bcd
Fiodar Motin <ordeh@mail.ru>
2018-04-07 18:26:43 +0300

    test

--- deploy_appveyor.sh
+++ deploy_appveyor.sh
@@ -2,18 +2,12 @@
 #export APPVEYOR_TOKEN="nchltpd0dyswqfwjuly4"
 #curl -H "Authorization: Bearer $APPVEYOR_TOKEN" -H "Content-Type: application/json" https://ci.appveyor.com/api/roles
 
-curl -X POST \
-  https://ci.appveyor.com/api/builds \
-  -H 'Authorization\": Bearer 3mb5uv6nmjl3l80ebsat' \
-  -H 'Content-Type: application/json' \
-  -d '{accountName = "ordeh"; projectSlug = "task-delivery"}'
+endpoint=https://ci.appveyor.com/api/builds
 
-#endpoint=https://api.travis-ci.com
-
-
-#function travis-api {
- # curl -s $endpoint$1 \
-  #     -H "Authorization: Bearer $APPVEYOR_TOKEN" \
-   #    -H 'Content-Type: application/json' \
-    #   "${@:2}"
-#}
+function travis-api {
+ curl -s $endpoint$1 \
+      -H "Authorization: Bearer $APPVEYOR_TOKEN" \
+      -H 'Content-Type: application/json' \
+      -d '{accountName = "ordeh"; projectSlug = "task-delivery"}' \
+      "${@:2}"
+}
